4350f298e350b9b7e893178b9e74ef994bf1de91
Bowei Zhang <bowei.zhang@airtable.com>
2023-01-01 17:35:09 -0800

    test command

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -7,10 +7,10 @@ get_shell_type() {
 echo "Running script"
 get_shell_type
 
-if [ -t 0 ]; then
-  echo "INTERACTIVE 2"
-else
+if [ ! -t 0 ]; then
   echo "NON-INTERACTIVE 2"
+else
+  echo "INTERACTIVE 2"
 fi
 
 if tty -s; then
@@ -19,3 +19,16 @@ else
   echo "NON-INTERACTIVE 3"
 fi
 
+echo $-
+if [[ $- == *i* ]]; then
+  echo "INTERACTIVE 4"
+else
+  echo "NON-INTERACTIVE 4"
+fi
+
+echo $PS1
+if [ -z "$PS1" ]; then
+  echo "NON-INTERACTIVE 5"
+else
+  echo "INTERACTIVE 5"
+fi
